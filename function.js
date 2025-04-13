34a1aa5736386b58ce2e9bb1cf6af0a125e5c071
gigi <giovanna.verissimo.bezerra@escola.pr.gov.br>
2025-04-12 22:19:09 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,11 @@
+function logar() {
+    var email=document.gptEleventbyID ('Email').value;
+    var login=document.gpteleventbyID ('Login').value;
+    var senha=document.gptEleventbyID ('Senha').value;
+ 
+    if (login =="adm" && senha=="123") { alert ('sucesso');
+    location.href="home.html";}
+ 
+    else{ alert ("Usuário ou senha incorreta");}
+}
+    
